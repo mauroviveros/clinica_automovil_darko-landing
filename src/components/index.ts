cb0253df20e3e5699506434b6ef99a2eeafae5e7
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-08-28 23:29:57 -0300

    chore: 🔧 add path patterns

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Brand } from "./Brand.astro";
+export { default as Navbar } from "./Navbar.astro";
+export { default as Partners } from "./Partners.astro";
+export { default as Section } from "./Section.astro";
+export { default as SEO } from "./SEO.astro";
---
+++ src/sections/index.ts
@@ -0,0 +1,3 @@
+export { default as Header } from "./Header.astro";
+export { default as Hero } from "./Hero.astro";
+export { default as Services } from "./Services.astro";
