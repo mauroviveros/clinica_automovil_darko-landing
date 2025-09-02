9075d1d7ab97445887e7ccd02d54f00f0fec1790
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-02 19:17:44 -0300

    feat: update contact information

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
@@ -9,5 +9,11 @@ export default defineConfig({
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
+  },
+  env: {
+    schema: {
+      GOOGLE_MAPS_API_KEY: envField.string({ context: 'server', access: 'secret' }),
+      GOOGLE_MAPS_PLACE_ID: envField.string({ context: 'server', access: 'secret' })
+    }
   }
 });
