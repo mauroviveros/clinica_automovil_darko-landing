8a8dcdf4468e2f31894abe154c3d62bc43e1d83b
Mauro Daniel viveros <maurod.viveros@gmail.com>
2025-09-02 17:06:33 -0300

    feat: :sparkles: add Testimonials section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Brand } from "./Brand.astro";
 export { default as Navbar } from "./Navbar.astro";
 export { default as Partners } from "./Partners.astro";
+export { default as Rating } from "./Rating.astro";
 export { default as Section } from "./Section.astro";
 export { default as SEO } from "./SEO.astro";
--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,3 +1,4 @@
 export { default as Faqs } from "./Faqs.astro";
 export { default as Hero } from "./Hero.astro";
 export { default as Services } from "./Services.astro";
+export { default as Testimonials } from "./Testimonials.astro";
